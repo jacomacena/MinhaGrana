d2803533ca71116ce13aef85746cb4c2486f4af8
Jacó Macena <jacog3@gmail.com>
2020-05-23 02:50:08 -0300

    update class

--- app/src/main/java/br/com/grupoprojetar/minhagrana/Login.java
+++ app/src/main/java/br/com/grupoprojetar/minhagrana/Login.java
@@ -2,23 +2,15 @@ package br.com.grupoprojetar.minhagrana;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
 import br.com.grupoprojetar.minhagrana.auth.Cadastro;
 import br.com.grupoprojetar.minhagrana.auth.Conexao;
 import br.com.grupoprojetar.minhagrana.auth.ResetPass;
-
 
 public class Login extends AppCompatActivity {
 
@@ -43,55 +35,41 @@ public class Login extends AppCompatActivity {
     private void acoesClicks() {
 
         //LOGIN
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = editEmail.getText().toString().trim();
-                String senha = editSenha.getText().toString().trim();
+        btnLogin.setOnClickListener(v -> {
+            String email = editEmail.getText().toString().trim();
+            String senha = editSenha.getText().toString().trim();
 
-
-
-                login(email,senha);
-            }
+            login(email,senha);
         });
 
         //CADASTRO
-        btnCadastrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent a = new Intent(getApplicationContext(), Cadastro.class);
-                startActivity(a);
-            }
+        btnCadastrar.setOnClickListener(v -> {
+            Intent a = new Intent(getApplicationContext(), Cadastro.class);
+            startActivity(a);
         });
 
         //RESET
-        txtResetPass.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent a = new Intent(Login.this, ResetPass.class);
-                startActivity(a);
-            }
+        txtResetPass.setOnClickListener(v -> {
+            Intent a = new Intent(Login.this, ResetPass.class);
+            startActivity(a);
         });
 
     }
 
     private void login(String email, String senha) {
         mAuth.signInWithEmailAndPassword(email,senha)
-                .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()){
-                            Intent a = new Intent(Login.this, MainActivity.class);
-                            startActivity(a);
-                        }else {
-                            logs("Email e/ou Senha incorretos! :(");
-                        }
+                .addOnCompleteListener(Login.this, task -> {
+                    if (task.isSuccessful()){
+                        Intent a = new Intent(Login.this, MainActivity.class);
+                        startActivity(a);
+                    }else {
+                        logs();
                     }
                 });
     }
 
-    private void logs(String s) {
-        Toast.makeText(Login.this,s,Toast.LENGTH_LONG).show();
+    private void logs() {
+        Toast.makeText(Login.this, "Email e/ou Senha incorretos! :(",Toast.LENGTH_LONG).show();
     }
 
     //inicializacao de toda a estrutura da tela (login.xml)
