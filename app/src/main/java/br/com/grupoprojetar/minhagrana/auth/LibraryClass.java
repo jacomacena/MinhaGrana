a0c998edbfaa238218b96fc22cf1cdddb801e7bd
Jacó Macena <jacog3@gmail.com>
2020-05-23 02:40:28 -0300

    update class

--- app/src/main/java/br/com/grupoprojetar/minhagrana/auth/LibraryClass.java
+++ app/src/main/java/br/com/grupoprojetar/minhagrana/auth/LibraryClass.java
@@ -3,12 +3,12 @@ package br.com.grupoprojetar.minhagrana.auth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public final class LibraryClass {
+final class LibraryClass {
     private static DatabaseReference firebase;
 
     private LibraryClass(){}
 
-    public static DatabaseReference getFirebase(){
+    static DatabaseReference getFirebase(){
         if( firebase == null ){
             firebase = FirebaseDatabase.getInstance().getReference();
         }
