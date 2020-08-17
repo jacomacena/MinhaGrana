156364e604b08bc98d520e9ecb799ee25be85932
Jacó Macena <jacog3@gmail.com>
2020-08-17 17:50:53 -0300

    insert classes

---
+++ app/src/main/java/br/com/grupoprojetar/minhagrana/Credits.java
@@ -0,0 +1,14 @@
+package br.com.grupoprojetar.minhagrana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Credits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+    }
+}
